1da8e6256e9074673bd946c0858d10b3a2e86252
Ana Acuna <anasofacuna@gmail.com>
2019-11-19 23:12:02 -0500

    made sure variables match id

--- new.php
+++ new.php
@@ -3,7 +3,7 @@ include('connect-db.php');
 
 $name = mysqli_real_escape_string($connection, htmlspecialchars($_POST['name']));
 $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
-$comments = mysqli_real_escape_string($connection, htmlspecialchars($_POST['comments']));
+$comments = mysqli_real_escape_string($connection, htmlspecialchars($_POST['comment']));
 
 mysqli_query($connection, "INSERT INTO rome (name, email, comments) VALUES ('$name', '$email', '$comments')");
 
